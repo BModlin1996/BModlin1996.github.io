bf5e62a13b47abcd9a8e62fb8f3d4e5ceb35bc80
Bailey <baileymodlin@aol.com>
2018-05-02 17:28:25 -0400

    Created seperate pages for each info tab

---
+++ menu.js
@@ -0,0 +1,21 @@
+﻿function openTab(evnt, tabName) {
+    switch (tabName) {
+        case "Home":
+            window.location.href = "index.html";
+            break; 
+        case "About":
+            window.location.href = "about.html";
+            break;
+        case "Software":
+            window.location.href = "software.html";
+            break; 
+        case "Hardware":
+            window.location.href = "hardware.html";
+            break; 
+        case "Resume":
+            window.location.href = "resume.html";
+            break; 
+        case "Contact":
+            window.location.href = "contact.html";
+    }
+}
---
+++ softwareMenu.js
@@ -0,0 +1,12 @@
+﻿var menu = document.getElementsByClassName("softwareMenu");
+for (var i = 0; 0 < menu.length; i++) {
+    menu[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
